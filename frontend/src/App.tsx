1a4b4a95e2adae5f8b31e1506213a25eb56e6141
Thaciara Justti <taciara0105@gmail.com>
2021-09-06 23:02:52 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import NavBar from "components/NavBar";
+import footer from "components/footer";
+import DataTable from "components/DataTable";
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá Mundo!!</h1>
+      <DataTable />
     </div>
+    <footer />
+    </>
   );
 }
 
