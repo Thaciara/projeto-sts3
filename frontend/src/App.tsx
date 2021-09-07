099de1d8ab2535453d13386a2707701e9e81c28b
Thaciara Justti <taciara0105@gmail.com>
2021-09-07 09:29:22 -0300

    Project clean

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import NavBar from "components/NavBar";
-import footer from "components/footer";
 import DataTable from "components/DataTable";
 import BarChart from "components/BarChart";
 import DonutChart from "components/DonutChart";
+import footer from "components/footer";
 
 function App() {
   return (
