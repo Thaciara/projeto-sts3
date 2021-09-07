d67dc806d56c0cc46b20506d128b791d3faac921
Thaciara Justti <taciara0105@gmail.com>
2021-09-07 09:56:34 -0300

    Project clean

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -5,9 +5,9 @@ const NavBar = () => {
                 <p className="text-light">App desenvolvido por <a href="https://github.com/Thaciara" target="_blank" rel="noreferrer">Thaciara Justti</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br/>
         Evento promovido pela escola DevSuperior: <a href="https://instagram.com/thacijustti" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
-                </div>
-</footer>
+            </div>
+        </footer>
 
-    );
+);
 }
 export default NavBar;
